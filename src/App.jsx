8f5b0828e2b7350c475d1c28e92e300e1d1265c4
sven <svenrisse0@gmail.com>
2022-11-02 09:48:28 +0100

    feat and styling of Roll button

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
-import Dice from "./components/Dice"
+import Dice from "./components/Dice";
 
 function App() {
-  const [diceNumbers, setDiceNumbers] = useState(allNewDice());
+    const [diceNumbers, setDiceNumbers] = useState(allNewDice());
 
-  function allNewDice() {
-    const newDice = [];
-    for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6))
+    function allNewDice() {
+        const newDice = [];
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6));
+        }
+        return newDice;
     }
-    return newDice;
-  }
 
-  const dices = diceNumbers.map(number => {
-    return <Dice value={number} />
-  })
-  
+    const dices = diceNumbers.map((number) => {
+        return <Dice value={number} />;
+    });
 
-  return (
-    <main className="app-container">
-      <div className="dice-container">
-        {dices}
-      </div>
-    </main>
-  )
+    function rollDice() {
+      setDiceNumbers(allNewDice())
+    }
+
+    return (
+        <main className="app-container">
+            <div className="dice-container">{dices}</div>
+            <button className="app-button" onClick={rollDice}>Roll</button>
+        </main>
+    );
 }
 
-export default App
+export default App;
