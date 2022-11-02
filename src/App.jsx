f9b629b9580fe958fa6f0af212560e2a5ec251fa
sven <svenrisse0@gmail.com>
2022-11-02 08:57:26 +0100

    feat: dice now rendered dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import Dice from "./components/Dice"
 
 function App() {
+  const [diceNumbers, setDiceNumbers] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice;
+  }
+
+  const dices = diceNumbers.map(number => {
+    return <Dice value={number} />
+  })
+  
+
   return (
     <main className="app-container">
       <div className="dice-container">
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
+        {dices}
       </div>
     </main>
   )
