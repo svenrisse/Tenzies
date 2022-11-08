cb4d4042f01e6ab076221ee714d28e49df4b915f
sven <svenrisse0@gmail.com>
2022-11-08 23:58:56 +0100

    added dice component

---
+++ src/components/Dice.tsx
@@ -0,0 +1,25 @@
+import "./Dice.css";
+
+interface IDice {
+    key: string;
+    value: number;
+    isHeld: boolean;
+    handleClick: () => void;
+}
+
+export default function Dice(props: IDice) {
+
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    return (
+        <div
+            className="dice-face"
+            style={styles}
+            onClick={() => props.handleClick}
+            
+        >
+            <h2 className="dice-numbers">{props.value}</h2>
+        </div>
+    );
+}
